2532f172ce2938d8c233c7987bc7368ca5f3fed4
jrodriguez <jircdev@gmail.com>
2022-03-25 12:30:17 -0300

    files deleted

--- prueba-juan/modules/home/ts/controller.tsx
+++
@@ -1,9 +0,0 @@
-import {PageReactWidgetController} from '@beyond-js/kernel/react-widget/ts';
-import {Page} from "./views/page";
-
-export /*bundle*/
-class Controller extends PageReactWidgetController {
-    get Widget() {
-        return Page;
-    }
-}
