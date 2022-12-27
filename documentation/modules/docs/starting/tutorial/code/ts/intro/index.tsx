604e817f8ae713d0694824b905b0798a9eac193a
veD-tnayrB <Brayanmc.contacto@gmail.com>
2022-12-26 20:33:29 -0400

    Feature: Reproduced the bug mdx bug for testing

--- documentation/modules/docs/starting/tutorial/code/ts/intro/index.tsx
+++ documentation/modules/docs/starting/tutorial/code/ts/intro/index.tsx
@@ -3,12 +3,14 @@ import { hmr, Document } from '@beyond/docs/components/html';
 
 import { module } from 'beyond_context';
 import { tpls } from '../templates';
+import ES from './es.mdx';
 
 export /*bundle*/ function TutorialIntro() {
 	return (
 		<>
-			<Document specifier={module.specifier} textId="intro" />
-			<Document specifier={module.specifier} textId="starting" tpls={tpls.starting} nextLinks={['/docs/tutorial/first-module', '/docs/tutorial/backend']} />
+			{/* <Document specifier={module.specifier} textId="intro" /> */}
+			{/* <Document specifier={module.specifier} textId="starting" tpls={tpls.starting} nextLinks={['/docs/tutorial/first-module', '/docs/tutorial/backend']} /> */}
+			<ES />
 		</>
 	);
 }
