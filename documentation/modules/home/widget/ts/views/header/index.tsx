a73841edeeabdb00e66ffe8ecba4b1fad0afd3b9
Julio Rodriguez <jircdev@gmail.com>
2022-12-27 18:23:34 -0300

    feat: add new svgs

--- documentation/modules/home/widget/ts/views/header/index.tsx
+++ documentation/modules/home/widget/ts/views/header/index.tsx
@@ -1,42 +1,43 @@
-import * as React from 'react';
-import '@beyond/docs/header-menu.widget';
-import { useHomeContext, HeaderContext } from '../contexts';
-import { SVGImage } from './svgs/svg-image';
-import { CDN } from './svgs/cdn';
+import * as React from "react";
+import "@beyond/docs/header-menu.widget";
+import { useHomeContext, HeaderContext } from "../contexts";
+import { SVGImage } from "./svgs/svg-image";
+import { CDN } from "./svgs/cdn";
 
-import { Buttons } from './buttons';
-import { PublishSVG } from './svgs/publish';
-import { ServerSVG } from './svgs/server';
+import { Buttons } from "./buttons";
+import { PublishSVG } from "./svgs/publish";
+import { ServerSVG } from "./svgs/server";
+import { BackgroundContainer } from "./background/background-container";
 
 export function Header() {
-	const container = React.useRef(null);
-	const [slide, setSlide] = React.useState('dev');
-	const {
-		texts: { header: texts },
-	} = useHomeContext();
-	const images = {
-		prod: CDN,
-		publish: PublishSVG,
-		dev: ServerSVG,
-	};
+    const container = React.useRef(null);
+    const [slide, setSlide] = React.useState("dev");
+    const {
+        texts: { header: texts },
+    } = useHomeContext();
+    const images = {
+        prod: CDN,
+        publish: PublishSVG,
+        dev: ServerSVG,
+    };
 
-	return (
-		<HeaderContext.Provider value={{ setSlide, slide }}>
-			<header className="container home__header" ref={container}>
-				<div className="welcome__text">
-					<h1>{texts.title}</h1>
+    return (
+        <HeaderContext.Provider value={{ setSlide, slide }}>
+            <header className="container home__header" ref={container}>
+                <div className="welcome__text">
+                    <h1>{texts.title}</h1>
+                    <p>{texts.subtitle}</p>
+                    {/* <h1>El framework para programación modular basado en paquetes universales</h1> */}
+                </div>
 
-					<p>{texts.subtitle}</p>
-					{/* <h1>El framework para programación modular basado en paquetes universales</h1> */}
-				</div>
-
-				<figure className="header__banners">
-					<figcaption>
-						<Buttons />
-					</figcaption>
-					<SVGImage src={images[slide]} />
-				</figure>
-			</header>
-		</HeaderContext.Provider>
-	);
+                <figure className="header__banners">
+                    <figcaption>
+                        <Buttons />
+                    </figcaption>
+                    <SVGImage src={images[slide]} />
+                </figure>
+                <BackgroundContainer />
+            </header>
+        </HeaderContext.Provider>
+    );
 }
--- pruebas/modules/layouts/main/ts/controller.tsx
+++
@@ -1,9 +0,0 @@
-import {ReactWidgetController} from '@beyond-js/kernel/react-widget/ts';
-import {Layout} from './view/layout';
-
-export /*bundle*/
-class Controller extends ReactWidgetController {
-    get Widget() {
-        return Layout;
-    }
-}
