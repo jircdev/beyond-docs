775b4562afd446692036a3d2988df9e8e8ec7204
veD-tnayrB <Brayanmc.contacto@gmail.com>
2022-12-30 10:47:50 -0400

    Fix: Solve the old kernel version issue

--- documentation/modules/contents/page/widget/ts/views/page.tsx
+++ documentation/modules/contents/page/widget/ts/views/page.tsx
@@ -1,17 +1,17 @@
-import * as React from "react";
-import { hmr, ContentsPage } from "@beyond/docs/contents-data.code";
-import Component from "./test.mdx";
+import * as React from 'react';
+import { hmr, ContentsPage } from '@beyond/docs/contents-data.code';
+import Component from './test.mdx';
 
 export function Page({ uri, component }): JSX.Element {
-    const propsContent = uri.vars.get("content");
-    const [hmrChanged, setHmr] = React.useState(performance.now());
-    const sub = uri.vars.get("sub");
-    const contentId = !["", undefined, null].includes(propsContent) ? propsContent : "intro";
+	const propsContent = uri.vars.get('content');
+	const [hmrChanged, setHmr] = React.useState(performance.now());
+	const sub = uri.vars.get('sub');
+	const contentId = !['', undefined, null].includes(propsContent) ? propsContent : 'intro';
 
-    return (
-        <div>
-            <Component />
-            <ContentsPage />
-        </div>
-    );
+	return (
+		<div>
+			<Component />
+			<ContentsPage />
+		</div>
+	);
 }
