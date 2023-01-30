e061bf8e85af36f299b0999eeaa20d42ea415e1c
Julio Rodriguez <jircdev@gmail.com>
2023-01-30 09:00:14 -0300

    feat: Add new universal development image.
    Add new image with complete background and solve structure of the home header.

--- context-translate.md
+++ context-translate.md
@@ -1,21 +1,6 @@
 Estamos trabajando en la documentacion de BeyondJS.
 
-BeyondJS es un framework de nueva generación diseñado para
-facilitar la creación de módulos JavaScript universales listos para ser distribuidos como paquetes NPM, simplificando el proceso de desarrollo y garantizando una experiencia homogénea en cualquier entorno de ejecución JavaScript.
-
-Características y ventajas:
-
-Utiliza Typescript y trabaja directamente con su API para abstraer al programador del proceso de compilación/transpilado a JavaScript y brindar un manejo de errores optimizado y rápido.
-Compatible con los paquetes NPM que sigan el estándar de Node, permitiendo su integración con un gran número de librerías.
-Los proyectos o aplicaciones en BeyondJS son manejados como un paquete javascript que puede ser publicado en NPM de forma directa.
-El package.json en BeyondJS es un archivo fundamental para la configuración y gestión de proyectos, con funciones comunes y agregando entry points adicionales para gestionar los paquetes de forma universal.
-Los módulos en BeyondJS son la unidad básica de desarrollo, divididos en dos conceptos: Módulos Internos y Módulos. Los Módulos Internos representan el concepto de módulos JavaScript tradicionales, mientras que los Módulos en BeyondJS representan la unidad de distribución y pueden contener módulos internos.
-Aplicaciones:
-
-Proyectos web
-Aplicaciones móviles híbridas
-Aplicaciones de escritorio híbridas
-Microservicios y backends monolíticos
-Extensiones
-Microfrontends o widgets para integrarlos en proyectos existentes
-En resumen, BeyondJS es una herramienta que permite crear módulos universales y distribuirlos como paquetes NPM, simplificando el proceso de desarrollo y garantizando una experiencia homogénea en cualquier entorno de ejecución JavaScript.
+Voy a pasarte un texto para que lo traduzcas a ingles.
+El texto esta hecho en formato mdx, que es una extension de markdown
+por favor genera el contendo respetando las etiquetas
+de los componentes y agrega la opcion de copiar y pegar.
--- documentation/modules/home/widget/ts/views/sections/universal-development.tsx
+++ documentation/modules/home/widget/ts/views/sections/universal-development.tsx
@@ -1,32 +1,32 @@
-import React from "react";
-import { BeyondButton } from "@beyond/ui/form";
-import { useHomeContext } from "../contexts";
-import { SVGImage } from "@beyond/docs/components/html";
-import { UniversalDevSVG } from "./svg-universal-dev";
+import React from 'react';
+import { BeyondButton } from '@beyond/ui/form';
+import { useHomeContext } from '../contexts';
+import { SVGImage } from '@beyond/docs/components/html';
+import { UniversalDevSVG } from './svg-universal-dev';
 
 export function UniversalDevelopment() {
-    const {
-        texts: { universalDevelopment: texts },
-    } = useHomeContext();
+	const {
+		texts: { universalDevelopment: texts },
+	} = useHomeContext();
 
-    return (
-        <section className="page-section container two-columns overflow-visible">
-            <SVGImage src={UniversalDevSVG} />
+	return (
+		<section className="page-section container two-columns universal__section">
+			<SVGImage src={UniversalDevSVG} />
 
-            <div className="content">
-                <header className="section__header">
-                    <span className="pretitle">{texts.pretitle}</span>
-                    <h3 className="main__title">{texts.title}</h3>
-                    <h5 className="sub__title">{texts.subTitle}</h5>
-                </header>
+			<div className="content">
+				<header className="section__header">
+					<span className="pretitle">{texts.pretitle}</span>
+					<h3 className="main__title">{texts.title}</h3>
+					<h5 className="sub__title">{texts.subTitle}</h5>
+				</header>
 
-                <p>{texts.concept}</p>
+				<p>{texts.concept}</p>
 
-                <div className="flex-container flex-center-y">
-                    <BeyondButton className="primary btn-outline">{texts.startNow}</BeyondButton>
-                    <a href="#">{texts.moreInfo}</a>
-                </div>
-            </div>
-        </section>
-    );
+				<div className="flex-container flex-center-y">
+					<BeyondButton className="primary btn-outline">{texts.startNow}</BeyondButton>
+					<a href="#">{texts.moreInfo}</a>
+				</div>
+			</div>
+		</section>
+	);
 }
